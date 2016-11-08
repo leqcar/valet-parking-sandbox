8ed4a8426a11fe405934bd53762c21a19be597ca
leqcar <leqcar@gmail.com>
2016-11-09 02:59:53 +0800

    updated

--- valet-api-gateway/src/main/java/com/leqcar/ValetApiGatewayApplication.java
+++ valet-api-gateway/src/main/java/com/leqcar/ValetApiGatewayApplication.java
@@ -3,12 +3,14 @@ package com.leqcar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class VehicleServiceApplication {
+@EnableZuulProxy
+public class ValetApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VehicleServiceApplication.class, args);
+		SpringApplication.run(ValetApiGatewayApplication.class, args);
 	}
 }
